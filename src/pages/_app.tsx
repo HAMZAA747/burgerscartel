bd3ae5efc01f219612ded3749d16317e226f0bf2
HAMZAA747 <alihamza321hz@gmail.com>
2025-05-12 02:45:28 +0500

    chore: bump _app.tsx to trigger redeploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,1 @@
-// src/pages/_app.tsx
-
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+// 🚀 bump to force redeploy
